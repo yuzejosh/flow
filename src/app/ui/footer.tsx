0abc282fa2b4764f235ae4b86f06e794e7f55e9c
josherh <josh.yuze@student.unimelb.edu.au>
2024-05-05 15:40:24 +1000

    added landing page and other pages

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>Footer</p>
-    </footer>
+    <footer className="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left border border-black">
+  <div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+    © 2024 Copyright:
+    <a href="https://youtube.com/"> Flow</a>
+  </div>
+</footer>
   );
 }
 
